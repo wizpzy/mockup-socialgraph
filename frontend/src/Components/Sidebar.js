4cfafe71bef5f533202accf7fc42af6c350d5159
AngwaraS <cherrygg88@gmail.com>
2024-07-04 11:17:02 +0700

    Update filter and search
    -add search
    -add 2 filter
    -change default of location keep in parameter at App

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css"; // Import CSS for styling
-
-import { IoChevronForwardSharp } from "react-icons/io5";
+import { IoChevronBackSharp, IoChevronForwardSharp } from "react-icons/io5";
 
 const Sidebar = ({ onClose, children }) => {
+  const [isVisibleSidebar, setVisibleSidebar] = useState(true);
+
+  const handleSidebar = () => {
+    setVisibleSidebar(!isVisibleSidebar);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-content">
-        <div className="sidebar-header">
-          <button className="close-btn" onClick={onClose}>
-            &times;
+    <>
+      {isVisibleSidebar && (
+        <div className="sidebar">
+          <div className="sidebar-content">
+            <div className="sidebar-header">
+              <button className="close-btn" onClick={onClose}>
+                &times;
+              </button>
+              <div className="company-pic">
+                <div></div>
+              </div>
+              <div className="company-logo">
+                <div></div>
+              </div>
+            </div>
+            <div className="sidebar-body">{children}</div>
+          </div>
+          <div className="hide-button">
+            <button className="hide-icon" onClick={handleSidebar}>
+              <IoChevronBackSharp />
+            </button>
+          </div>
+        </div>
+      )}
+      {!isVisibleSidebar && (
+        <div className="show-button">
+          <button className="show-icon" onClick={handleSidebar}>
+            <IoChevronForwardSharp />
           </button>
         </div>
-        <div className="sidebar-body">{children}</div>
-      </div>
-      <div className="show-hide-button">
-          <div><IoChevronForwardSharp /></div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
