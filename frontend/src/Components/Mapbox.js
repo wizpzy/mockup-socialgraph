55060153430a78a4485f99fe679b3566909f150a
wizpzy <wizpzy@gmail.com>
2024-07-04 18:28:23 +0700

    feat: Query image

--- frontend/src/Components/Mapbox.js
+++ frontend/src/Components/Mapbox.js
@@ -22,7 +22,7 @@ const Mapbox = ({ selectedIndustry, selectedProvince, location }) => {
     const fetchData = async () => {
       try {
         const response = await Axios.get(
-          "http://localhost:1337/api/companies/?populate[0]=Location&populate[1]=Industry"
+          "http://localhost:1337/api/companies/?populate[0]=Location&populate[1]=Industry&populate[2]=Image"
         );
         //console.log(response);
         setQueryData(response.data.data);
@@ -60,6 +60,7 @@ const Mapbox = ({ selectedIndustry, selectedProvince, location }) => {
             industry: item.attributes.Industry.data
               ? item.attributes.Industry.data.attributes.Name
               : null,
+            imageUrl: item.attributes.Image.data ? item.attributes.Image.data.attributes.url : null,
             cluster: false,
             id: item.id,
           },
@@ -264,7 +265,8 @@ const Mapbox = ({ selectedIndustry, selectedProvince, location }) => {
         .setHTML(
           `<h2> ${feature.properties.title} </h2>
           <h3> ${feature.properties.industry} </h3>
-          <p> ${feature.properties.description} </p>`
+          <p> ${feature.properties.description} </p>
+          ${feature.properties.imageUrl ? `<img src="${'http://localhost:1337'+feature.properties.imageUrl}" alt="${feature.properties.title}" style="max-width:100%;">` : ''}`
         )
         .addTo(map.current);
       popupRef.current = popup;
