ed506f060ba2d38b3fd6d215b0b27c815933c7b8
AngwaraS <cherrygg88@gmail.com>
2024-07-11 18:04:25 +0700

    Revert "Change Typography and Add Product Sidebar"
    This reverts commit 5cc62d9ae485853f2ee68e538f509144f3fd2a7c.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Axios from "axios";
 import Header from "./Components/Header";
 import Mapbox from "./Components/Mapbox";
 import Sidebar from "./Components/Sidebar";
-import ProductSidebar from "./Components/Sidebar_product"
 import "./App.css";
 
 const App = () => {
@@ -49,7 +48,6 @@ const App = () => {
         location={location}
       />
       <Sidebar/>
-      {/* <ProductSidebar/> */}
     </div>
   );
 };
